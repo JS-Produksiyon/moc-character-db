b75cf74731c3c201b59c42c32a1cee69e49b895b
JMW (HS9-WSL) <jmw@hawke-ai.com>
2024-07-23 17:27:59 +0300

    updated database infrastructure

--- ED Karakter Dizini MySQL-1721744810.sql
+++ ED Karakter Dizini MySQL-1721744810.sql
@@ -19,9 +19,11 @@ CREATE TABLE IF NOT EXISTS `characters` (
 	`personality` text NOT NULL UNIQUE,
 	`image_head` blob NOT NULL UNIQUE,
 	`image_body` blob NOT NULL UNIQUE,
+	`animation_status` int NOT NULL,
 	`residence` int NOT NULL,
 	`marital_status` int NOT NULL,
 	`episodes` int NOT NULL UNIQUE,
+	`new_field` int NOT NULL,
 	`acted_by` int NOT NULL,
 	PRIMARY KEY (`id`)
 );
@@ -40,14 +42,22 @@ CREATE TABLE IF NOT EXISTS `relationships` (
 	PRIMARY KEY (`id`)
 );
 
+CREATE TABLE IF NOT EXISTS `char_to_ep` (
+	`id` int AUTO_INCREMENT NOT NULL UNIQUE,
+	`character` int NOT NULL,
+	`episode` int NOT NULL,
+	PRIMARY KEY (`id`)
+);
 
 
-ALTER TABLE `characters` ADD CONSTRAINT `characters_fk8` FOREIGN KEY (`residence`) REFERENCES `residence`(`id`);
 
-ALTER TABLE `characters` ADD CONSTRAINT `characters_fk10` FOREIGN KEY (`episodes`) REFERENCES `episodes`(`id`);
+ALTER TABLE `characters` ADD CONSTRAINT `characters_fk9` FOREIGN KEY (`residence`) REFERENCES `residence`(`id`);
 
-ALTER TABLE `characters` ADD CONSTRAINT `characters_fk11` FOREIGN KEY (`acted_by`) REFERENCES `actors`(`id`);
+ALTER TABLE `characters` ADD CONSTRAINT `characters_fk13` FOREIGN KEY (`acted_by`) REFERENCES `actors`(`id`);
 
 ALTER TABLE `relationships` ADD CONSTRAINT `relationships_fk1` FOREIGN KEY (`main_character`) REFERENCES `characters`(`id`);
 
 ALTER TABLE `relationships` ADD CONSTRAINT `relationships_fk2` FOREIGN KEY (`other_character`) REFERENCES `characters`(`id`);
+ALTER TABLE `char_to_ep` ADD CONSTRAINT `char_to_ep_fk1` FOREIGN KEY (`character`) REFERENCES `characters`(`id`);
+
+ALTER TABLE `char_to_ep` ADD CONSTRAINT `char_to_ep_fk2` FOREIGN KEY (`episode`) REFERENCES `episodes`(`id`);

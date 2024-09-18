8152c5e96374d06f0d7c2ad8dfbb3dbe8967ee6f
JMW (HS9) <jmw@hawke-ai.com>
2024-09-18 10:06:17 +0300

    added relationship management

--- app/static/ui.navigation.js
+++ app/static/ui.navigation.js
@@ -20,17 +20,31 @@ var hashMgr = function () {
         console.log("Current location: " + window.location.hash);
         $(".md-load.lion").fadeIn(); /* replace with spinner activation code */
         $(".md-layer").fadeOut();
-        
+        $(".md-add-button").fadeOut();
+        $(".nav-link").removeClass('active');
+
         /* parse hash */
         var h = window.location.hash.split('/');
         console.log(h);
-        if (h[1] == 'character' && h.length == 2) {
+        /* NOTE: All of this code below is temporary until the various objects that handle the display of the views is written!
+                 It is just here so that I can easily navigate between views. */
+        if (h[1] == 'character' && h.length > 2) {
             /* replace with load character detail code */
             $("#char_detail").fadeIn();
+            $("#add_char_btn").fadeIn();
+            $("#nav_characters").addClass("active");
         } else if (h[1] == 'episodes') {
             $("#episode_list").fadeIn();
+            $("#add_ep_btn").fadeIn();
+            $("#nav_episodes").addClass("active");
+        } else if (h[1] == 'relationships') {
+            $("#relations_list").fadeIn();
+            $("#add_rel_btn").fadeIn();
+            $("#nav_relations").addClass("active");
         } else {
             $("#char_list").fadeIn();
+            $("#add_char_btn").fadeIn();
+            $("#nav_characters").addClass("active");
         }
         /* replace with spinner deactivation code */
         $(".md-load").fadeOut();

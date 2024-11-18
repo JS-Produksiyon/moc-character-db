043d4cfd843693dcff833d69ec3011a6c842c80c
JMW (HS9) <jmw@hawke-ai.com>
2024-11-18 12:17:54 +0300

    updated batch file to work in both non-dev and dev mode

--- startmoc.bat
+++ startmoc.bat
@@ -1,3 +1,9 @@
 @echo off
-cd app
-flask --debug run --extra-files static/style.css:static/*.js
+REM Check if the first argument is "dev"
+if "%1"=="dev" (
+    cd app
+    flask --debug run --extra-files static/style.css:static/*.js
+) else (
+    cd app
+    flask run --extra-files static/style.css:static/*.js
+)
